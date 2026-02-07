f6b1ca395bd0abcb772feb783640708b7ed74149
Fabio Colella <fcole90@gmail.com>
2026-02-07 12:12:47 +0100

    Set nextConfig basePath to formyl

--- packages/main/next.config.ts
+++ packages/main/next.config.ts
@@ -3,7 +3,7 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isDev ? '' : '/swiftpost-site-template',
+  basePath: isDev ? '' : '/formyl',
 };
 
 export default nextConfig;
